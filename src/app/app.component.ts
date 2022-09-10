e385d1600292b4b95d3c83df2728390552d8b824
RomarioRibeiro <romryjane@hotmai.com>
2022-09-10 17:23:25 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,20 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+
+interface ICalculoModele {
+  salario: number;
+  inss: number;
+}
+
+
+
+interface IResult {
+  salarioBruto: number;
+  Classificacao: number;
+  grauSalarioMinimo: number;
+  Adiatamento: number;
+}
 
 @Component({
   selector: 'app-root',
@@ -7,4 +23,97 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'holerite';
+
+model:IResult  = {
+ salarioBruto: 0,
+ Classificacao: 0,
+ grauSalarioMinimo: 0,
+ Adiatamento: 0
+
+}
+
+
+
+result: any;
+
+constructor() {
+
+}
+onsubmit(form = NgForm) {
+  if (this.model.salarioBruto <=0 ) {
+    return;
+  }
+  const salarioBruto = this.getClassificacao(this.model.salarioBruto);
+  const Classificacao = this.getClassificacao;
+  const grauSalarioMinimo = this.getsalarioMinimo(this.model.salarioBruto);
+  const Adiatamento = this.getAdiant(this.model.salarioBruto);
+
+
+console.log(salarioBruto);
+console.log(grauSalarioMinimo);
+console.log(Adiatamento)
+
+
+
+this.result = {
+  salarioBruto,
+  Classificacao,
+  grauSalarioMinimo,
+  Adiatamento
+
+
+
+}
+
+}
+
+
+
+//calculeSalario(salario:number, inss:number) {
+//  return salario * 7.5 / 100;
+//}
+
+
+
+getClassificacao(salarioBruto: number) {
+  if(salarioBruto < 1212) {
+    return salarioBruto * 7.5 / 100;
+  }else if(salarioBruto >= 1212.00 && salarioBruto <=2427.35){
+    const v =  (salarioBruto - 1212) * 9 / 100;
+    const r = 1212 * 7.5 / 100;
+    const l = v + r;
+
+    return l;
+  }else{
+    return 'teste';
+  }
+}
+
+getsalarioMinimo(salarioBruto: number) {
+  if(salarioBruto < 1212.00) {
+    return '0';
+  }else if(salarioBruto >= 1212.00 && salarioBruto <=2427.35){
+    const v =  (salarioBruto - 1212) * 9 / 100;
+    const r = 1212 * 7.5 / 100;
+    const l = v + r;
+    const g = salarioBruto - l;
+    return g;
+  }else{
+    return 'teste';
+  }
+}
+
+
+getAdiant(salarioBruto: number) {
+  if(salarioBruto < 1212.00) {
+    const g = salarioBruto * 40 / 100;
+    return g;
+  }else if(salarioBruto >= 1212.00 && salarioBruto <=2427.35){
+    const g = salarioBruto * 40 / 100;
+    return g;
+  }else{
+    return 'teste';
+  }
+}
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import {CardModule} from 'primeng/card';
+import {InputTextModule} from 'primeng/inputtext';
+import {InputMaskModule} from 'primeng/inputmask';
+import {ButtonModule} from 'primeng/button';
+import {InputNumberModule} from 'primeng/inputnumber';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    CardModule,
+    InputTextModule,
+    InputMaskModule,
+    FormsModule,
+    ButtonModule,
+    InputNumberModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
